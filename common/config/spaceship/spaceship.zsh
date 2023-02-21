76bef02e226901bd33d7c2428dfbe67838eff1f7
Zach Kelling <z@zeekay.io>
2023-02-21 02:35:44 -0600

    Add spaceship plugin configuration

---
+++ common/config/spaceship/spaceship.zsh
@@ -0,0 +1,16 @@
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+SPACESHIP_CHAR_SYMBOL_ROOT="▼"
+
+SPACESHIP_VI_MODE_SHOW=true
+SPACESHIP_VI_MODE_INSERT=⏷
+SPACESHIP_VI_MODE_NORMAL=⏶
+SPACESHIP_VI_MODE_COLOR=grey
+SPACESHIP_CHAR_SYMBOL=" "
+
+# Add a custom vi-mode section to the prompt
+# See: https://github.com/spaceship-prompt/spaceship-vi-mode
+spaceship add --before char vi_mode
+spaceship remove gcloud
+
+SPACESHIP_PROMPT_PREFIXES_SHOW=false
